d9b9d0ed5fc6c08abfc02d1236a80db2b39cc4a7
Paweł Cegielski <skok2022@gmail.com>
2024-05-08 19:27:07 +0200

    Bit of cleanup

--- ReLu.jl
+++ ReLu.jl
@@ -3,11 +3,6 @@ forward(::BroadcastedOperator{typeof(ReLu)}, input) = let
     return max.(input, 0)
 end
 
-backward(node::BroadcastedOperator{typeof(ReLu)}, input::Array{Float32, 1}, g) = let
-    tuple(g .* (input .>= 0))
-end
-
-
-backward(node::BroadcastedOperator{typeof(ReLu)}, input::Array{Float32, 3}, g) = let
-    tuple(g .* (input .>= 0))
+backward(node::BroadcastedOperator{typeof(ReLu)}, input, g) = let
+    tuple( (input .>= 0) .* g)
 end
